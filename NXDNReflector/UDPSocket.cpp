289f79f6b88492214b63aa45142c39f012c34ce4
Jonathan Naylor <naylorjs@yahoo.com>
2021-09-12 20:23:25 +0100

    Hopefully fix the IP display in the reflector.

--- NXDNReflector/UDPSocket.cpp
+++ NXDNReflector/UDPSocket.cpp
@@ -168,22 +168,22 @@ char* CUDPSocket::display(const sockaddr_storage& addr, char* buffer, unsigned i
 
 	switch (addr.ss_family) {
 		case AF_INET: {
-				struct sockaddr_in* in4 = (struct sockaddr_in*)&addr;
-				::inet_ntop(AF_INET, &in4->sin_addr, buffer, length);
-				::sprintf(buffer + ::strlen(buffer), ":%u", in4->sin_port);
-			}
-			break;
+			struct sockaddr_in* in4 = (struct sockaddr_in*)&addr;
+			::inet_ntop(AF_INET, &in4->sin_addr, buffer, length);
+			::sprintf(buffer + ::strlen(buffer), ":%u", in4->sin_port);
+		}
+		break;
 
-		case AF_INET6: {
-				struct sockaddr_in6* in6 = (struct sockaddr_in6*)&addr;
-				::inet_ntop(AF_INET6, &in6->sin6_addr, buffer, length);
-				::sprintf(buffer + ::strlen(buffer), ":%u", in6->sin6_port);
-			}
-			break;
+	case AF_INET6: {
+			struct sockaddr_in6* in6 = (struct sockaddr_in6*)&addr;
+			::inet_ntop(AF_INET6, &in6->sin6_addr, buffer, length);
+			::sprintf(buffer + ::strlen(buffer), ":%u", in6->sin6_port);
+		}
+		break;
 
-		default:
-			::strcpy(buffer, "Unknown");
-			break;
+	default:
+		::strcpy(buffer, "Unknown");
+		break;
 	}
 
 	return buffer;
